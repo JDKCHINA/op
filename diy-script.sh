94073f10da63fefe5e3b23e879d2037023bc518a
498110811 <498110811@qq.com>
2024-10-25 17:49:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -44,7 +44,7 @@ git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
+git clone https://github.com/kenzok78/luci-theme-design package/luci-theme-design
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
